aeb5ddef031c84bf1bbc3cae93a6b990d86a59ef
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2017-03-13 18:57:00 -0700

    Probando todos los cafés sin condimentos ...

---
+++ Starbuzz/test-scond/testsc/AllTestsSC.java
@@ -0,0 +1,11 @@
+package testsc;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TDarkRoast.class, TDecaf.class, TEspresso.class, THouseBlend.class })
+public class AllTestsSC {
+
+}
